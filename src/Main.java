ad303c99b4d8b8382e13ba9d1b8d37172a07207c
Nata R <natasha.melanis@yandex.ru>
2023-03-27 18:44:01 +0300

    Java code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int account = 100; // сумма на счету
+        int refill = 1300; // сумма пополнения
+
+        int bonus;
+        if (refill < 1000) {
+            bonus = 0;
+            
+        } else {
+            bonus = refill / 100;  
+        }
+        int total = account + refill + bonus; // итоговая сумма на счету с учетом бонуса
+        
+        System.out.println ("Итоговый счёт: " + total);
+        System.out.println ("Бонусы: " + bonus);
+        
+
     }
 }
